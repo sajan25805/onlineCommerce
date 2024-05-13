bd81820e874fed08113ebaa21cb5ef66b4399271
Samyukta Khadka <ms.samyuktakdk@gmail.com>
2024-05-13 16:43:36 +0545

    Dashboard Page Done

--- frontend/src/pages/AdminDashboardPage.jsx
+++ frontend/src/pages/AdminDashboardPage.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import AdminHeader from "../components/Layout/AdminHeader";
-import AdminSideBar from "../components/Admin/Layout/AdminSideBar";
-import AdminDashboardMain from "../components/Admin/AdminDashboardMain";
+import AdminHeader from "../components/Layout/AdminHeader"; // Importing the AdminHeader component
+import AdminSideBar from "../components/Admin/Layout/AdminSideBar"; // Importing the AdminSideBar component
+import AdminDashboardMain from "../components/Admin/AdminDashboardMain"; // Importing the AdminDashboardMain component
 
+// Defining the AdminDashboardPage component
 const AdminDashboardPage = () => {
   return (
     <div>
+      {/* Rendering the AdminHeader component */}
       <AdminHeader />
       <div className="w-full flex">
         <div className="flex items-start justify-between w-full">
+          {/* Rendering the AdminSideBar component */}
           <div className="w-[80px] 800px:w-[330px]">
             <AdminSideBar active={1} />
           </div>
+          {/* Rendering the AdminDashboardMain component */}
           <AdminDashboardMain />
         </div>
       </div>
@@ -19,4 +23,4 @@ const AdminDashboardPage = () => {
   );
 };
 
-export default AdminDashboardPage;
+export default AdminDashboardPage; // Exporting the AdminDashboardPage component
