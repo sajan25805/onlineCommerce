ae37d26cf1fdc4323c08e734be934f296dd256cd
sajan25805 <sajanrajmainali99@gmail.com>
2024-05-20 08:51:16 +0545

    Bug For Button Color Fixed

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -352,7 +352,7 @@ const Signup = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+                className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-black  hover:bg-slate-900"
               >
                 Submit
               </button>
