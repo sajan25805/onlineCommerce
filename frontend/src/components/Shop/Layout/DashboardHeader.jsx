52f12e0864820554afd5703b96a16b1529a5c7e7
sajan25805 <sajanrajmainali99@gmail.com>
2024-05-22 22:29:59 +0545

    Logo Issue Fixed

--- frontend/src/components/Shop/Layout/DashboardHeader.jsx
+++ frontend/src/components/Shop/Layout/DashboardHeader.jsx
@@ -5,6 +5,8 @@ import { FiPackage, FiShoppingBag } from "react-icons/fi";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { BiMessageSquareDetail } from "react-icons/bi";
+import vintageLogo from "../../../images/vinclogo.png";
+
 
 const DashboardHeader = () => {
   const { seller } = useSelector((state) => state.seller);
@@ -12,10 +14,7 @@ const DashboardHeader = () => {
     <div className="w-full h-[80px] bg-white shadow sticky top-0 left-0 z-30 flex items-center justify-between px-4">
       <div>
         <Link to="/dashboard">
-          <img
-            src="https://shopo.quomodothemes.website/assets/images/logo.svg"
-            alt=""
-          />
+          <img src={vintageLogo} alt="My Image" height="50" width="50" />
         </Link>
       </div>
       <div className="flex items-center">
