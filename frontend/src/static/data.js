d379b77e796e06d3ac58d6b9a018dc8dba1cca4a
Hemangi <np03cs4s230013@heraldcollege.edu.np>
2024-05-22 18:51:40 +0545

    static data changed

--- frontend/src/static/data.js
+++ frontend/src/static/data.js
@@ -1,116 +1,116 @@
-// navigation Data
-export const navItems = [{
-    title: "Home",
-    url: "/",
-  },
-  {
-    title: "Best Selling",
-    url: "/best-selling",
-  },
-  {
-    title: "Products",
-    url: "/products",
-  },
-  {
-    title: "Events",
-    url: "/events",
-  },
-];
+  // navigation Data
+  export const navItems = [{
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "Best Selling",
+      url: "/best-selling",
+    },
+    {
+      title: "Products",
+      url: "/products",
+    },
+    {
+      title: "Events",
+      url: "/events",
+    },
+  ];
 
 
-// categories data
-export const categoriesData = [{
-    id: 1,
-    title: "Tops",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/a0/c0/2b/a0c02b67254e60a838452eb8c9de8a3d.jpg"
-  },
-  {
-    id: 2,
-    title: "Bottoms",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/c9/6e/f9/c96ef9c413a77eccf10429eb74453868.jpg"
-  },
-  {
-    id: 3,
-    title: "Accesories",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/236x/5c/01/5b/5c015b5e7096bf8c773841a882626486.jpg"
-  },
-  {
-    id: 4,
-    title: "Clothes",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/32/6d/ab/326dab181093f6dc358f6b3e34cb1c4c.jpg",
-  },
-  {
-    id: 5,
-    title: "Footwear",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/53/29/28/532928580a75d3ed4b006ab82899c48e.jpg",
-  },
-  {
-    id: 6,
-    title: "Dresses",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/43/da/ba/43daba9ec9ef33748efb0f1e6323af8b.jpg",
-  },
-  {
-    id: 7,
-    title: "Formal Wear",
-    subTitle: "",
-    image_Url: "",
-  },
+  // categories data
+  export const categoriesData = [{
+      id: 1,
+      title: "Tops",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/a0/c0/2b/a0c02b67254e60a838452eb8c9de8a3d.jpg"
+    },
+    {
+      id: 2,
+      title: "Bottoms",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/c9/6e/f9/c96ef9c413a77eccf10429eb74453868.jpg"
+    },
+    {
+      id: 3,
+      title: "Accesories",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/236x/5c/01/5b/5c015b5e7096bf8c773841a882626486.jpg"
+    },
+    {
+      id: 4,
+      title: "Clothes",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/32/6d/ab/326dab181093f6dc358f6b3e34cb1c4c.jpg",
+    },
+    {
+      id: 5,
+      title: "Footwear",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/53/29/28/532928580a75d3ed4b006ab82899c48e.jpg",
+    },
+    {
+      id: 6,
+      title: "Dresses",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/43/da/ba/43daba9ec9ef33748efb0f1e6323af8b.jpg",
+    },
+    {
+      id: 7,
+      title: "Formal Wear",
+      subTitle: "",
+      image_Url: "",
+    },
 
-  {
-    id: 9,
-    title: "Men Wears",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/564x/ff/00/00/ff00009421652bef9f54af6ab6521df4.jpg",
-  },
-  {
-    id: 10,
-    title: "Female Wears",
-    subTitle: "",
-    image_Url: "https://i.pinimg.com/736x/d0/b6/f3/d0b6f349336d00d7c05633e69b8a76eb.jpg",
-  },
-];
-
-
-export const footerProductLinks = [{
-    name: "About us",
-    link: "/about"
-  },
-  {
-    name: "Careers",
-    link: "/carrers"
-  },
-  {
-    name: "Store Locations",
-  },
-  {
-    name: "Our Blog",
-  },
-  {
-    name: "Reviews",
-  },
-];
+    {
+      id: 9,
+      title: "Men Wears",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/564x/ff/00/00/ff00009421652bef9f54af6ab6521df4.jpg",
+    },
+    {
+      id: 10,
+      title: "Female Wears",
+      subTitle: "",
+      image_Url: "https://i.pinimg.com/736x/d0/b6/f3/d0b6f349336d00d7c05633e69b8a76eb.jpg",
+    },
+  ];
 
 
+  export const footerProductLinks = [{
+      name: "About us",
+      link: "/about"
+    },
+    {
+      name: "Careers",
+      link: "/carrers"
+    },
+    {
+      name: "Store Locations",
+    },
+    {
+      name: "Our Blog",
+    },
+    {
+      name: "Reviews",
+    },
+  ];
 
-export const footerSupportLinks = [{
-    name: "FAQ",
-  },
-  {
-    name: "Reviews",
-  },
-  {
-    name: "Contact Us",
-  },
-  {
-    name: "Shipping",
-  },
-  {
-    name: "Live chat",
-  },
-];
+
+
+  export const footerSupportLinks = [{
+      name: "FAQ",
+    },
+    {
+      name: "Reviews",
+    },
+    {
+      name: "Contact Us",
+    },
+    {
+      name: "Shipping",
+    },
+    {
+      name: "Live chat",
+    },
+  ];
